b753545eed01769cdbeea9d3929b04957e44eda5
pavel <pavelrazrushitel1ads@mail.ru>
2025-03-29 14:19:23 +0700

    ㅤ

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+import "./header.js";
+import "./navigation.js";
+import "./video-section.js";
+import "./featured-offer.js";
+import "./sliders.js";
+import "./database.js";
+import "./footer.js";
+
+// MOBILE SCRIPTS
+import "../mobile-scripts/mobile-budget-section.js";
+import "../mobile-scripts/mobile-footer.js";
+import "../mobile-scripts/mobile-navigation.js";
